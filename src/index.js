b493a0eccf541eb1c3a5bfd2e030f9ccedc96720
Dzmitry <87767424+dzmitrySkr@users.noreply.github.com>
2022-02-05 14:54:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,74 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let obj = {
+    a: '.-',
+    b: '-...',
+    c: '-.-.',
+    d: '-..',
+    e: '.',
+    f: '..-.',
+    g: '--.',
+    h: '....',
+    i: '..',
+    j: '.---',
+    k: '-.-',
+    l: '.-..',
+    m: '--',
+    n: '-.',
+    o: '---',
+    p: '.--.',
+    q: '--.-',
+    r: '.-.',
+    s: '...',
+    t: '-',
+    u: '..-',
+    v: '...-',
+    w: '.--',
+    x: '-..-',
+    y: '-.--',
+    z: '--..',
+    ' ': 's',
+  }
+
+  let miniarray = []
+  let ss = []
+  let array = result.split('');
+  
+  array.forEach(el => {
+    
+    ss = [];
+    
+   let a = obj[el];
+   let b = a.split('')
+
+   console.log(b);
+   for(i=0; i< 10 - (b.length *2); i++){
+     ss.push(0);
+   }
+   
+   
+   b.forEach(((elem, index) => {
+
+  
+    if (elem === '.'){
+      ss.push(10)
+      }
+    if (elem === '-') {
+      ss.push(11)
+    } 
+
+    if (elem === 's'){
+      ss = ['**********']
+      miniarray.push(ss)
+    }else if(index === b.length-1){
+      miniarray.push(ss)
+    }
+    
+   }))
+  });
+
+ return miniarray.join('').split(',').join('');
 }
 
 module.exports = {
